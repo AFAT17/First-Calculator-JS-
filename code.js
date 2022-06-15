1464cedda01edf00a70d57c1a2ffff3d528e9547
Andres Arango <102338872+AFAT17@users.noreply.github.com>
2022-06-14 21:57:20 -0500

    Update code.js

--- code.js
+++ code.js
@@ -1,5 +1,20 @@
 
 
+
+
+const sumar = (num1,num2)=>{
+    return parseInt(num1)+parseInt(num2);
+}
+const restar = (num1,num2)=>{
+    return parseInt(num1)-parseInt(num2);
+}
+const dividir = (num1,num2)=>{
+    return parseInt(num1)/parseInt(num2);
+}
+const multiplicar = (num1,num2)=>{
+    return parseInt(num1)*parseInt(num2);
+}
+
 alert("¿que operacion deseas realizar?");
 let operacion = prompt("1: suma, 2: resta, 3: dividir, 4: multiplicar");
 
