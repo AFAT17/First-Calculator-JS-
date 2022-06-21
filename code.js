6b7b799c74229acde2e8c2cd6dace20b73c81e4c
Andres Arango <102338872+AFAT17@users.noreply.github.com>
2022-06-20 21:37:28 -0500

    Update code.js

--- code.js
+++ code.js
@@ -1,7 +1,4 @@
 
-
-
-
 const sumar = (num1,num2)=>{
     return parseInt(num1)+parseInt(num2);
 }
@@ -15,37 +12,43 @@ const multiplicar = (num1,num2)=>{
     return parseInt(num1)*parseInt(num2);
 }
 
-alert("¿que operacion deseas realizar?");
+alert("Welcome To The The Calculator Of The NASA")
+let nombre = prompt("¿Cual es tu nombre?")
+alert(`¿dime ${nombre} que operacion deseas realizar?`);
 let operacion = prompt("1: suma, 2: resta, 3: dividir, 4: multiplicar");
 
 if (operacion == 1){
-    let numero1 = prompt("primer numero para sumar");
-    let numero2 = prompt("segundo numero para sumar");
+    let numero1 = prompt(`${nombre} dime el primer numero para sumar`);
+    let numero2 = prompt(`${nombre} dime el segundo numero para sumar`);
     resultado = sumar(numero1,numero2);
-    alert(`tu resultado es ${resultado}`);
+    alert(`${nombre} segun The The Calculator Of The NASA tu resultado es`);
+    document.write(`<h1><b>${resultado}</b></h1>`);
 }
 
 else if (operacion == 2){
-    let numero1 = prompt("primer numero para restar");
-    let numero2 = prompt("segundo numero para restar");
+    let numero1 = prompt(`${nombre} dime el primer numero para restar`);
+    let numero2 = prompt(`${nombre} dime el segundo numero para restar`);
     resultado = restar(numero1,numero2);
-    alert(`tu resultado es ${resultado}`);
+    alert(`${nombre} segun The The Calculator Of The NASA tu resultado es`);
+    document.write(`<h1><b>${resultado}</b></h1>`);
 }
 
 else if (operacion == 3){
-    let numero1 = prompt("primer numero para dividir");
-    let numero2 = prompt("segundo numero para dividir");
+    let numero1 = prompt(`${nombre} dime el primer numero para dividir`);
+    let numero2 = prompt(`${nombre} dime el segundo numero para dividir`);
     resultado = dividir(numero1,numero2);
-    alert(`tu resultado es ${resultado}`);
+    alert(`${nombre} segun The The Calculator Of The NASA tu resultado es`);
+    document.write(`<h1><b>${resultado}</b></h1>`);
 }
 
 else if (operacion == 4){
-    let numero1 = prompt("primer numero para multiplicar");
-    let numero2 = prompt("segundo numero para multiplicar");
+    let numero1 = prompt(`${nombre} dime el primer numero para multiplicar`);
+    let numero2 = prompt(`${nombre} dime el segundo numero para multiplicar`);
     resultado = multiplicar(numero1,numero2);
-    alert(`tu resultado es ${resultado}`);
+    alert(`${nombre} segun The The Calculator Of The NASA tu resultado es`);
+    document.write(`<h1><b>${resultado}</b></h1>`);
 }
 
 else {
-    alert("No se pudo realizar la operacion")
+    alert(`${nombre} No se pudo realizar la operacion`)
 }
